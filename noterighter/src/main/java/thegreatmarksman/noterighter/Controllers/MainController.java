fce5f6325c958a4d1a2acfdd2597e36a054c6ded
TheGreatMarksman <marcusedooley@yahoo.com>
2024-11-30 15:01:52 -0800

    more set up

---
+++ noterighter/src/main/java/thegreatmarksman/noterighter/Controllers/MainController.java
@@ -0,0 +1,14 @@
+package thegreatmarksman.noterighter.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.ui.Model;
+
+import jakarta.servlet.http.HttpSession;;
+
+public class MainController{
+    @GetMapping("/")
+    public String getPage(Model model, HttpSession session) {
+        return "page";
+    }
+}
