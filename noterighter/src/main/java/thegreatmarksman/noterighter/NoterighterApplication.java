e1f4b78478aa568e9e9d2f8c3d5f2c5d024aa49a
TheGreatMarksman <marcusedooley@yahoo.com>
2024-11-30 14:37:39 -0800

    set up

---
+++ noterighter/src/main/java/thegreatmarksman/noterighter/NoterighterApplication.java
@@ -0,0 +1,13 @@
+package thegreatmarksman.noterighter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoterighterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoterighterApplication.class, args);
+	}
+
+}
---
+++ noterighter/src/test/java/thegreatmarksman/noterighter/NoterighterApplicationTests.java
@@ -0,0 +1,13 @@
+package thegreatmarksman.noterighter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoterighterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
