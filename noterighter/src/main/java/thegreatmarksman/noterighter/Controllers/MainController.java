b3efbb10d993cc024964ddc69e686c76e772e15a
TheGreatMarksman <marcusedooley@yahoo.com>
2024-12-05 19:20:38 -0800

    added main page

--- noterighter/src/main/java/thegreatmarksman/noterighter/Controllers/MainController.java
+++ noterighter/src/main/java/thegreatmarksman/noterighter/Controllers/MainController.java
@@ -1,11 +1,12 @@
 package thegreatmarksman.noterighter.Controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import javax.servlet.http.HttpSession;
 
+@Controller
 public class MainController{
     @GetMapping("/")
     public String getPage(Model model, HttpSession session) {
